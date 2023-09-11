1ad0e013a056626cb2e954d1c04063663220c0ad
nyctophiliac123 <nyctophiliac399@gmail.com>
2023-09-11 22:46:32 +0530

    Second

---
+++ app/src/main/java/com/example/instagramapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.instagramapp
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.statusBarColor= Color.TRANSPARENT
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,signUpActivity::class.java))
+            finish()
+        },3000)
+    }
+}
