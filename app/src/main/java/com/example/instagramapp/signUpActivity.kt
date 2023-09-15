2f13a3f65d8549a6c3a140e3c9100713f8c8152a
nyctophiliac123 <nyctophiliac399@gmail.com>
2023-09-15 22:23:21 +0530

    signUp_page_third

--- app/src/main/java/com/example/instagramapp/signUpActivity.kt
+++ app/src/main/java/com/example/instagramapp/signUpActivity.kt
@@ -2,10 +2,91 @@ package com.example.instagramapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.instagramapp.Models.User
+import com.example.instagramapp.databinding.ActivitySignUpBinding
+import com.example.instagramapp.utils.USER_NODE
+import com.example.instagramapp.utils.USER_PROFILE_FOLDER
+import com.example.instagramapp.utils.uploadImage
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class signUpActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
+
+    lateinit var user: User
+    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        uri->
+        uri?.let {
+            uploadImage(uri, USER_PROFILE_FOLDER) {
+                if (it==null) {
+
+                } else {
+                    user.image=it
+                    binding.profileImage.setImageURI(uri)
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+        user = User()
+
+        binding.signUpBtn.setOnClickListener {
+            if (binding.name.editText?.text.toString().equals("") or
+                binding.email.editText?.text.toString().equals("") or
+                binding.password.editText?.text.toString().equals("")
+            ) {
+                Toast.makeText(
+                    this@signUpActivity,
+                    "Please fill all the required fields",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else {
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.email.editText?.text.toString(),
+                    binding.password.editText?.text.toString()
+                ).addOnCompleteListener { result ->
+
+                    if (result.isSuccessful) {
+                        user.name = binding.name.editText?.text.toString()
+                        user.password = binding.password.editText?.text.toString()
+                        user.email = binding.email.editText?.text.toString()
+                        Firebase.firestore.collection(USER_NODE)
+                            .document(Firebase.auth.currentUser!!.uid).set(user)
+                            .addOnSuccessListener {
+                                Toast.makeText(
+                                    this@signUpActivity,
+                                    "SignUp successful :)",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+
+                    } else {
+                        Toast.makeText(
+                            this@signUpActivity,
+                            result.exception?.localizedMessage,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+
+
+        }
+
+        binding.addImage.setOnClickListener {
+            launcher.launch("image/*")
+        }
     }
 }
